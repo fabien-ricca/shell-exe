99bead61e615e26092cf584bb54d33d24d052374
QJoly <qjoly@users.noreply.github.com>
2022-10-03 20:03:16 +0200

    Utilisation d'une regex pour la condition

--- Job9/accessrights.sh
+++ Job9/accessrights.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/nix/store/fq9y3qc1vczbs2wqslz0cxccrrk71iy1-bash-interactive-5.1-p16/bin/bash
 
 # On attribut le fchier à une variable pour simplifier plus loin
-file="/home/padawan/script/shell-exe/Job9/userlist.csv"
+file="./userlist.csv"
 
 # On démarre la boucle, on attribue les variables pour chaque champs
 while IFS="," read -r Id Prenom Nom Mdp Role
 do
 # Si l'id est un entier on crée un utlisateur avec les données récupérées
-    if [ "$Id" -eq "$Id" ] 2>/dev/null;
+    if [[ "$Id" =~ [0-9]{1,4} ]];
     then
-        useradd -u $Id -p $Mdp ${Prenom,,}
+        useradd -u $Id -p $Mdp ${Prenom}
         echo "L'utilisateur $Prenom a bien été ajouté."
 # si son role est Admin on l'ajoute au groupe sudo
             if [ ${Role:0:5} = "Admin" ];
